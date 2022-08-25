9c4bc5c03dfeb7e761ca842ee689c9dfb7fa6ea8
voronov <voronov54@gmail.com>
2022-08-25 17:17:33 +0700

    TLS settings for greetings service

--- pkg/services/greeter/helloworld.go
+++ pkg/services/greeter/helloworld.go
@@ -2,11 +2,15 @@ package greeter
 
 import (
 	context "context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"time"
 
 	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
@@ -16,6 +20,42 @@ type HelloService struct {
 	connection   *grpc.ClientConn
 	client       GreeterClient
 	queryTimeout time.Duration
+}
+
+func LoadTLSCredentialsForClient(certPath string) (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile(certPath)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
+func LoadTLSCredentialsForServer(certPath, keyPath string) (credentials.TransportCredentials, error) {
+	// Load server's certificate and private key
+	serverCert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.NoClientCert,
+	}
+
+	return credentials.NewTLS(config), nil
 }
 
 func CreateHelloService(serverHost string) *HelloService {
