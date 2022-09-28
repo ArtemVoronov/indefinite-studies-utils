3badc8b64f2dd4c145731934174e908bc633f33c
voronov <voronov54@gmail.com>
2022-09-28 18:04:26 +0700

    added notifications grpc client/server (part 2)

---
+++ pkg/services/notifications/notifications.go
@@ -0,0 +1,71 @@
+package notifications
+
+import (
+	context "context"
+	"fmt"
+	"time"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type NotificationsGRPCService struct {
+	serverHost   string
+	dialOptions  []grpc.DialOption
+	connection   *grpc.ClientConn
+	client       NotificationsServiceClient
+	queryTimeout time.Duration
+}
+
+func CreatePostsGRPCService(serverHost string, creds *credentials.TransportCredentials) *NotificationsGRPCService {
+	var opts []grpc.DialOption
+	if creds != nil {
+		opts = append(opts, grpc.WithTransportCredentials(*creds))
+	} else {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
+	return &NotificationsGRPCService{
+		serverHost:   serverHost,
+		dialOptions:  opts,
+		queryTimeout: 30 * time.Second,
+	}
+}
+
+func (s *NotificationsGRPCService) connect() error {
+	conn, err := grpc.Dial(s.serverHost, s.dialOptions...)
+	if err != nil {
+		return fmt.Errorf("unable to connect to '%v', error: %v", s.serverHost, err)
+	}
+	s.connection = conn
+	s.client = NewNotificationsServiceClient(conn)
+	return nil
+}
+
+func (s *NotificationsGRPCService) Shutdown() error {
+	if s.connection != nil {
+		return s.connection.Close()
+	}
+	return nil
+}
+
+func (s *NotificationsGRPCService) SendEmail(sender string, recepient string, body string) error {
+
+	if s.connection == nil {
+		err := s.connect()
+		if err != nil {
+			return fmt.Errorf("could not GetPost: %v", err)
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+
+	_, err := s.client.SendEmail(ctx, &SendEmailRequest{Sender: sender, Recepient: recepient, Body: body})
+	if err != nil {
+		return fmt.Errorf("could not GetPost: %v", err)
+	}
+
+	return nil
+}
