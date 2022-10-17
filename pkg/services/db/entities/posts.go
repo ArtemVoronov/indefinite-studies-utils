72c1f951d0e63b135c7ab60cf78904402537ba78
voronov <voronov54@gmail.com>
2022-10-17 18:32:55 +0700

    added posts state constants

---
+++ pkg/services/db/entities/posts.go
@@ -0,0 +1,9 @@
+package entities
+
+const (
+	POST_STATE_NEW           string = "NEW"
+	POST_STATE_ON_MODERATION string = "ON_MODERATION"
+	POST_STATE_PUBLISHED     string = "PUBLISHED"
+	POST_STATE_BLOCKED       string = "BLOCKED"
+	POST_STATE_DELETED       string = "DELETED"
+)
