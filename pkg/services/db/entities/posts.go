7ef09a98e4a8157f37b09526c989538c285be7d5
voronov <voronov54@gmail.com>
2022-10-18 21:24:17 +0700

    unified post states, user roles

--- pkg/services/db/entities/posts.go
+++ pkg/services/db/entities/posts.go
@@ -7,3 +7,7 @@ const (
 	POST_STATE_BLOCKED       string = "BLOCKED"
 	POST_STATE_DELETED       string = "DELETED"
 )
+
+func GetPossiblePostStates() []string {
+	return []string{POST_STATE_NEW, POST_STATE_ON_MODERATION, POST_STATE_PUBLISHED, POST_STATE_BLOCKED, POST_STATE_DELETED}
+}
--- pkg/services/db/entities/users.go
+++ pkg/services/db/entities/users.go
@@ -5,3 +5,7 @@ const (
 	USER_ROLE_RESIDENT string = "RESIDNET"
 	USER_ROLE_GI       string = "GI"
 )
+
+func GetPossibleUserRoles() []string {
+	return []string{USER_ROLE_OWNER, USER_ROLE_RESIDENT, USER_ROLE_GI}
+}
