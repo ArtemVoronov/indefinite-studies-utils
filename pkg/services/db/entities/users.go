24e1111f5b83d01d4a6efac593ef283610c53e6e
voronov <voronov54@gmail.com>
2024-03-24 10:31:42 +0700

    fix misspellings

--- pkg/services/db/entities/users.go
+++ pkg/services/db/entities/users.go
@@ -2,7 +2,7 @@ package entities
 
 const (
 	USER_ROLE_OWNER    string = "OWNER"
-	USER_ROLE_RESIDENT string = "RESIDNET"
+	USER_ROLE_RESIDENT string = "RESIDENT"
 	USER_ROLE_GI       string = "GI"
 )
 
